c144d9edb71846e16feadffe650f9da5fa68f658
Jory Jonathan <o22106917@info03-06.iut45.univ-orleans.fr>
2024-01-09 09:48:01 +0100

    init

---
+++ b/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/iut45/Etudiants/o22106917/WinHome/Multimedia/TD5
+# Build directory: /home/iut45/Etudiants/o22106917/WinHome/Multimedia/TD5/b
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test([=[SimpleTest]=] "td")
+set_tests_properties([=[SimpleTest]=] PROPERTIES  _BACKTRACE_TRIPLES "/home/iut45/Etudiants/o22106917/WinHome/Multimedia/TD5/CMakeLists.txt;70;add_test;/home/iut45/Etudiants/o22106917/WinHome/Multimedia/TD5/CMakeLists.txt;0;")
